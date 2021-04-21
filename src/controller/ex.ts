77123aa7934ddd471efc0bb8a47d444d81438ac3
ricky <rjbenavides@cpp.edu>
2021-04-20 19:37:12 -0700

    controller with some CRUD operations

---
+++ src/controller/ex.ts
@@ -0,0 +1,47 @@
+import { Request, Response, Router } from "express";
+
+import Example from "./../entities/Example";
+
+export const create = async (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  try {
+    const ex = new Example(name);
+    await ex.save();
+    return res.json(ex);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+export const read = async (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  try {
+    const ex = await Example.findOneOrFail(name);
+    return res.json(ex);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+export const getById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const ex = await Example.findOneOrFail(id);
+    return res.json(ex);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+export const update = async (req: Request, res: Response) => {};
+
+export const deleteEx = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
